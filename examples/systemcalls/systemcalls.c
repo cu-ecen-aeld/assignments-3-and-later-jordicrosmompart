1e8758c8b4cddee69906b9270a33e2154e7bd6a6
jordicrosmompart <jordi.cros.mompart@gmail.com>
2022-09-07 11:10:36 -0600

    Implemented part 1, unit_test runs successfully.

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,14 @@
 #include "systemcalls.h"
+#include <errno.h>
+#include <syslog.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#define	STDOUT	(1)
 
 /**
  * @param cmd the command to execute with system()
@@ -11,13 +21,52 @@ bool do_system(const char *cmd)
 {
 
 /*
- * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+	int ret_val = system(cmd);
+	
+	//If cmd is NULL, the value returned is interpreted differently
+	if(cmd == NULL)
+	{
+		//0 means that no shell is available
+		if(!ret_val)
+		{
+			syslog(LOG_ERR, "No shell available.");
+			return false;
+		}
+		else
+		{	
+			syslog(LOG_DEBUG, "Shell available.");
+			return true;
+		}
+	}
+	//cmd is not NULL, check return value and return accordingly
+	else
+	{
+		if(ret_val == -1)
+		{
+			//Check what error occurred and print it on syslog (default facility)
+			syslog(LOG_ERR, "Could not successfully execute the command \"%s\": %s", cmd, strerror(errno));
+			//Return false
+			return false;
+		}
+		else if(ret_val == 127)
+		{
+			//Unknown to the system() caller if the call returned 127 or the invocation failed
+			syslog(LOG_ERR, "Could not successfully execute the command \"%s\"", cmd);
+			//Return false, expecting that the process called does not return 127 on success
+			return false;
+		}
+		else
+		{
+			//Syslog the return value  (default facility)
+			syslog(LOG_DEBUG, "The command %s ran successfully and it returned %d.", cmd, ret_val);
+			return true;
+		}
+    }
+		
 }
 
 /**
@@ -40,17 +89,15 @@ bool do_exec(int count, ...)
     va_start(args, count);
     char * command[count+1];
     int i;
+    
+    //Load all the arguments to "command"
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 /*
- * TODO:
  *   Execute a system command by calling fork, execv(),
  *   and wait instead of system (see LSP page 161).
  *   Use the command[0] as the full path to the command to execute
@@ -58,10 +105,60 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	int fork_ret = fork();
+	if(fork_ret == -1)
+	{
+		syslog(LOG_ERR, "Unable to perform fork: %s.", strerror(errno));
+		return false;
+	}
+	else if(fork_ret == 0)
+	{
+		//Child process, execute the other process
+		execv(command[0], command);
+		syslog(LOG_ERR, "execv all failed: %s.", strerror(errno));
+		return false;
+	}
 
     va_end(args);
 
-    return true;
+	//Wait for the child process to finalize
+	int status;
+	int wait_ret = waitpid(fork_ret, &status, 0);
+	if(wait_ret == -1)
+	{
+		syslog(LOG_ERR, "waitpid() failed: %s.", strerror(errno));
+		return false;
+	}
+	else if(wait_ret == fork_ret)
+	{
+		if(WIFEXITED(status))
+		{
+			//Execution finished calling exit.
+			//The value matters to this function
+			if(WEXITSTATUS(status) != 0)
+			{
+				syslog(LOG_ERR, "Child process exited with a non-zero value."); 
+				return false;
+			}
+			else
+			{
+				syslog(LOG_DEBUG, "Child process finalized successfully.");
+			}
+		}
+		else
+		{
+			syslog(LOG_ERR, "Child process did not reach the end of its execution normally.");
+			return false;
+		}
+	}
+	else
+	{
+		syslog(LOG_ERR, "waitpid() returned an unexpected result.");
+    	return false;
+	}
+	
+	//This should never reach here, but the compiler complains
+	return false;
 }
 
 /**
@@ -86,14 +183,28 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
 
 
 /*
- * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
  *   redirect standard out to a file specified by outputfile.
  *   The rest of the behaviour is same as do_exec()
  *
 */
+	int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if(fd < 0)
+	{
+		syslog(LOG_ERR, "Could not create the file where stdout will be redirected: %s", strerror(errno));
+		return false;
+	}
+	if(dup2(fd, STDOUT) < 0)
+	{
+		syslog(LOG_ERR, "Could not redirect stdout to the file: %s", strerror(errno));
+		close(fd);
+		return false;
+	}
+	
+	//Call do_exec
+	bool exec_ret = do_exec(count, args);
 
     va_end(args);
 
-    return true;
+    return exec_ret;
 }
